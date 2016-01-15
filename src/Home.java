d8774155dba35baac8f01329334445cc6741f3c7
cngondo <devngondo@gmail.com>
2016-01-15 12:28:18 +0300

    Added ternary operator

--- src/Home.java
+++ src/Home.java
@@ -4,6 +4,10 @@
 public class Home {
 
     static public void main(String[]argy){
-
+        /*
+        * Create object and invoke the method
+        * */
+        Thinktank thinktank = new Thinktank();
+        thinktank.testnumbers();
     }
 }
---
+++ src/Thinktank.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ngondo on 1/15/16.
+ */
+public class Thinktank {
+    public void testnumbers(){
+        int aNum;
+        int anotherNum = 7;
+        /*
+        * Testing tenary operator
+        * check if anotherNum = 10. If it's true assign 5 to aNum
+        * else assign 70
+        * */
+        aNum = (anotherNum == 10)? 5: 70;
+
+        System.out.println("aNum = " + aNum);
+    }
+}
